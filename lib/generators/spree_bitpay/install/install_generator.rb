59387204adb8547940fa922f5b1bbbf965a25b01
Hamish Eisler <hamish@HamPay.local>
2014-08-04 00:51:27 -0700

    ci fixes

--- lib/generators/spree_bitpay/install/install_generator.rb
+++ lib/generators/spree_bitpay/install/install_generator.rb
@@ -2,8 +2,6 @@ module SpreeBitpay
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
-      class_option :auto_run_migrations, :type => :boolean, :default => false
-
       def add_javascripts
         append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_bitpay\n"
         append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_bitpay\n"
@@ -19,12 +17,7 @@ module SpreeBitpay
       end
 
       def run_migrations
-        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
-        if run_migrations
-          run 'bundle exec rake db:migrate'
-        else
-          puts 'Skipping rake db:migrate, don\'t forget to run it!'
-        end
+        run 'bundle exec rake db:migrate'
       end
     end
   end
