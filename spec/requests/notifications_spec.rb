9d8b934fcdbb05ef91112156da121117d667cd75
Hamish Eisler <heisler3030@users.noreply.github.com>
2014-08-11 23:16:37 -0400

    add skeleton for request specs

---
+++ spec/requests/notifications_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "Notifications" do
+	it "Handles 'paid' notification"
+	it "Handles 'confirmed' notification"
+	it "Handles Overpayment"
+	it "Handles Underpayment"
+	it "Handles 'invalid' notification"
+	it "Handles Late Payment"
+	it "Handles non-existent orders"
+	it "Handles non-existent payments"
+	it "Handles false 'paid' notifications"
+	it "Handles false 'confirmed' notifications"
+end
