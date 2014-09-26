bc57b3134448e3552972a635183e7bef47a85e23
Paul Daigle <paul@bitpay.com>
2014-09-26 15:56:48 -0400

    We no longer need the manual folder steps

--- testapp.sh
+++ testapp.sh
@@ -4,13 +4,3 @@ export RAILS_ENV=test
 
 rm -r spec/dummy
 bundle exec rake test_app
-
-# Append stylesheets/js manually until fix applied
-# https://github.com/spree/spree/issues/5157
-
-cd spec/dummy
-gsed -i $'7i\\\n \*\= require spree\/frontend\n' vendor/assets/stylesheets/spree/frontend/all.css
-gsed -i $'7i\\\n \*\= require spree\/backend\n' vendor/assets/stylesheets/spree/backend/all.css
-gsed -i $'10i\\\n\/\/\= require spree\/frontend\n' vendor/assets/javascripts/spree/frontend/all.js
-gsed -i $'10i\\\n\/\/\= require spree\/backend\n' vendor/assets/javascripts/spree/backend/all.js
-cd ../..
