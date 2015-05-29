334023b77b691dfa83e0e219b5b48029e028631c
Paul Daigle <paul@bitpay.com>
2015-05-29 10:15:31 -0400

    remove unused test

--- spec/features/create_new_bitpay_payment_method_spec.rb
+++ spec/features/create_new_bitpay_payment_method_spec.rb
@@ -9,17 +9,6 @@ feature "Creating a new BitPay Payment Method", js: true, type: :feature do
     expect(page).to have_selector('#bitpay_api_uri option[value="https://test.bitpay.com"]'), "No testnet dropdown"
   end
   
-  scenario 'the user is logged in to bitpay' do
-    login_bitpay ENV['BPDEVSERVER'], ENV['BPUSER'], ENV['BPPASSWORD']
-    login_admin
-    old_url = current_url
-    create_new_payment_method "Bitcoin", "Spree::PaymentMethod::BitPay"
-    select("Development", from: "bitcoin_network")
-    find_button("Authenticate with BitPay").click
-    #click the approval button
-    #do something else
-  end
-
   scenario 'the user is logged out of bitpay' do
     login_admin
     create_new_payment_method "Bitcoin", "Spree::PaymentMethod::BitPay"
